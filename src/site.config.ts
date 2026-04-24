1083c1cee2d9a70d64925c72b6a8f7fcbe0322c5
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2026-04-24 23:56:24 +0200

    refactor: configurate new site.config.ts

---
+++ src/site.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from './types'
+
+export const configSite: Config = {
+  title: 'Kointa Finance',
+  description:
+    'Aplicación web para la gestión de finanzas personales, diseñada para ayudar a las personas a llevar un control claro y visual de sus ingresos, gastos y categorías.',
+  lang: 'es',
+  author: 'bmmedina99',
+  url: 'https://localhost:5000',
+}
+
+export const navLinks = [
+  {
+    id: '0',
+    title: 'Características',
+    href: '#features',
+  },
+  {
+    id: '1',
+    title: 'Comenzar',
+    href: '#started',
+  },
+]
